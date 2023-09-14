4e7435513b780d9f4ca430d75f825230d4f73b35
Daniel Murberg <daniel.murberg@hyperisland.se>
2023-09-14 16:04:12 +0200

    cli

--- ftt/ftt.py
+++ ftt/ftt.py
@@ -14,15 +14,14 @@ def get_between(s, start_sub, end_sub):
 
 
 class TmuxTransfer:
-    def __init__(self, frwindow, towindow, frpane, topane, frfile, tofile):
+    def __init__(self, frwindow, towindow, frpane, topane):
         self.frwindow = frwindow
         self.towindow = towindow
         self.frpane = frpane
         self.topane = topane
-        self.frfile = frfile
-        self.tofile = tofile
 
-    def send_keys(self, pane, cmd, idx):
+    def send_keys(self, pane, cmd):
+        idx = self.id()
         run(
             [
                 "tmux",
@@ -33,12 +32,13 @@ class TmuxTransfer:
                 "Enter",
             ]
         )
+        return idx
 
-    def tosend_keys(self, cmd, idx):
-        self.send_keys(self.topane, cmd, idx)
+    def tosend_keys(self, cmd):
+        return self.send_keys(self.topane, cmd)
 
-    def frsend_keys(self, cmd, idx):
-        self.send_keys(self.frpane, cmd, idx)
+    def frsend_keys(self, cmd):
+        return self.send_keys(self.frpane, cmd)
 
     def get_buffer(self):
         res = run(["tmux", "show-buffer"], stdout=PIPE)
@@ -46,7 +46,7 @@ class TmuxTransfer:
         return res
 
     def capture_buffer(self, pane):
-        run(["tmux", "capture-pane", "-S", "-" , "-t", str(pane)])
+        run(["tmux", "capture-pane", "-S", "-", "-t", str(pane)])
 
     def id(self):
         return uuid.uuid4().hex
@@ -61,8 +61,7 @@ class TmuxTransfer:
 
     def check_file(self, path):
         """Check if file exists or if we can access it"""
-        idx = self.id()
-        self.frsend_keys(f"ls {path}", idx)
+        idx = self.frsend_keys(f"ls {path}")
 
         time.sleep(0.1)
         r = self.get_text(self.frpane, idx)
@@ -73,23 +72,22 @@ class TmuxTransfer:
 
     def save_file(self, path, data):
         """Save file to reciever
-        Outputs buffered data by sending multiple small parts by echoing to file        
+        Outputs buffered data by sending multiple small parts by echoing to file
         """
         i = 0
         s = 173
         tmpfile = self.id()
 
         while True:
-            idx = self.id()
             batch = data[i : i + s]
-            self.tosend_keys(f"echo '{batch}' >> /tmp/{tmpfile}", idx)
+            idx = self.tosend_keys(f"echo '{batch}' >> /tmp/{tmpfile}")
 
             i += s
             if i > len(data):
                 break
-        
-        self.tosend_keys(f"base64 -d /tmp/{tmpfile} > {path}", idx)
-        self.tosend_keys(f"rm /tmp/{tmpfile}", idx)
+
+        self.tosend_keys(f"base64 -d /tmp/{tmpfile} > {path}")
+        self.tosend_keys(f"rm /tmp/{tmpfile}")
 
     def transfer(self, fpath, tpath):
         data = self.fetch_file(fpath)
@@ -99,19 +97,17 @@ class TmuxTransfer:
         """Fetch file content to memory
         Decode file to base64 and output parts to tmux buffer
         """
-        
-        idx = self.id()
-        self.frsend_keys(f"base64 {file} | wc -l", idx)
+
+        idx = self.frsend_keys(f"base64 {file} | wc -l")
         nlines = int(self.get_text(self.frpane, idx))
-        
+
         buf = ""
         pointer = SIZE_BULK
         bulk = SIZE_BULK
         last = ""
         while True:
-            idx = self.id()
-            self.frsend_keys(
-                f"base64 {file} | head -n {pointer} | tail -n {bulk}", idx
+            idx = self.frsend_keys(
+                f"base64 {file} | head -n {pointer} | tail -n {bulk}"
             )
             time.sleep(0.1)
             r = self.get_text(self.frpane, idx)
@@ -123,15 +119,14 @@ class TmuxTransfer:
             # Last bulk
             if (pointer + SIZE_BULK) >= nlines:
                 bulk = nlines - pointer
-                pointer = nlines 
+                pointer = nlines
             else:
                 pointer += bulk
         return buf
 
     def has_base64(self):
         """Checks if sender has base64 binary installed"""
-        idx = self.id()
-        self.frsend_keys("which base64", idx)
+        idx = self.frsend_keys("which base64")
         time.sleep(0.1)
         if len(self.get_text(self.frpane, idx).strip()) > 0:
             return True
@@ -139,6 +134,12 @@ class TmuxTransfer:
         return False
 
 
-trans = TmuxTransfer(0, 0, 2, 1, 0, 0)
+def transfer_file(sp, rp, sf, rf):
+    trans = TmuxTransfer(0, 0, sp, rp)
+    trans.transfer(sf, rf)
 
-trans.transfer("seq", "/tmp/lol2")
+
+if __name__ == "__main__":
+    trans = TmuxTransfer(0, 0, 1, 2, 0, 0)
+
+    trans.transfer("/usr/bin/touch", "/tmp/tt")
